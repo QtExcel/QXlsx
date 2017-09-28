ac0277183f49b70a96cf1b1f179af8c81786a360
j2doll <j2doll@gmail.com>
2017-09-28 21:49:56 +0900

    test v2

--- QtXlsx/header/xlsxcell.h
+++ QtXlsx/header/xlsxcell.h
@@ -1,27 +1,28 @@
-/****************************************************************************
-** Copyright (c) 2013-2014 Debao Zhang <hello@debao.me>
-** All right reserved.
-**
-** Permission is hereby granted, free of charge, to any person obtaining
-** a copy of this software and associated documentation files (the
-** "Software"), to deal in the Software without restriction, including
-** without limitation the rights to use, copy, modify, merge, publish,
-** distribute, sublicense, and/or sell copies of the Software, and to
-** permit persons to whom the Software is furnished to do so, subject to
-** the following conditions:
-**
-** The above copyright notice and this permission notice shall be
-** included in all copies or substantial portions of the Software.
-**
-** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-** NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-** LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-** OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-** WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-**
-****************************************************************************/
+//--------------------------------------------------------------------
+//
+// MIT License
+// Copyright (c) 2017, j2doll
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//--------------------------------------------------------------------
+
 #ifndef QXLSX_XLSXCELL_H
 #define QXLSX_XLSXCELL_H
 
@@ -39,37 +40,38 @@ class WorksheetPrivate;
 
 class Q_XLSX_EXPORT Cell
 {
-    Q_DECLARE_PRIVATE(Cell)
+	Q_DECLARE_PRIVATE(Cell)
 public:
-    enum CellType {
-        BooleanType,      //t="b"
-        NumberType,       //t="n" (default)
-        ErrorType,        //t="e"
-        SharedStringType, //t="s"
-        StringType,       //t="str"
-        InlineStringType  //t="inlineStr"
-    };
+	enum CellType {
+		BooleanType,      //t="b"
+		NumberType,       //t="n" (default)
+		ErrorType,        //t="e"
+		SharedStringType, //t="s"
+		StringType,       //t="str"
+		InlineStringType  //t="inlineStr"
+	};
 
-    CellType cellType() const;
-    QVariant value() const;
-    Format format() const;
+	CellType cellType() const;
+	QVariant value() const;
+	QVariant readValue() const;
+	Format format() const;
+	
+	bool hasFormula() const;
+	CellFormula formula() const;
 
-    bool hasFormula() const;
-    CellFormula formula() const;
+	bool isDateTime() const;
+	QDateTime dateTime() const;
 
-    bool isDateTime() const;
-    QDateTime dateTime() const;
+	bool isRichString() const;
 
-    bool isRichString() const;
-
-    ~Cell();
+	~Cell();
 private:
-    friend class Worksheet;
-    friend class WorksheetPrivate;
+	friend class Worksheet;
+	friend class WorksheetPrivate;
 
-    Cell(const QVariant &data=QVariant(), CellType type=NumberType, const Format &format=Format(), Worksheet *parent=0);
-    Cell(const Cell * const cell);
-    CellPrivate * const d_ptr;
+	Cell(const QVariant &data=QVariant(), CellType type=NumberType, const Format &format=Format(), Worksheet *parent=0);
+	Cell(const Cell * const cell);
+	CellPrivate * const d_ptr;
 };
 
 QT_END_NAMESPACE_XLSX
