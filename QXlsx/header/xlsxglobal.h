f81e707f9f9958fb15d69eaf81124b3d4f95618e
Look Skyworker <j2doll@gmail.com>
2017-10-05 14:41:48 +0900

    set new namespace

--- QXlsx/header/xlsxglobal.h
+++ QXlsx/header/xlsxglobal.h
@@ -1,27 +1,23 @@
-/****************************************************************************
-** Copyright (c) 2013-2014 Debao Zhang <hello@debao.me>
-** All right reserved.
-**
-** Permission is hereby granted, free of charge, to any person obtaining
-** a copy of this software and associated documentation files (the
-** "Software"), to deal in the Software without restriction, including
-** without limitation the rights to use, copy, modify, merge, publish,
-** distribute, sublicense, and/or sell copies of the Software, and to
-** permit persons to whom the Software is furnished to do so, subject to
-** the following conditions:
-**
-** The above copyright notice and this permission notice shall be
-** included in all copies or substantial portions of the Software.
-**
-** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-** NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-** LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-** OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-** WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-**
-****************************************************************************/
+//--------------------------------------------------------------------
+//
+// QXlsx https://github.com/j2doll/QXlsx
+//
+// GPL License v3 https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//--------------------------------------------------------------------
+
 #ifndef XLSXGLOBAL_H
 #define XLSXGLOBAL_H
 #include <QtGlobal>
@@ -29,18 +25,7 @@
 #define QT_BEGIN_NAMESPACE_XLSX namespace QXlsx {
 #define QT_END_NAMESPACE_XLSX }
 #define QTXLSX_USE_NAMESPACE using namespace QXlsx;
-
-/*
-#if !defined(QT_STATIC) && !defined(XLSX_NO_LIB)
-#  if defined(QT_BUILD_XLSX_LIB)
-#    define Q_XLSX_EXPORT Q_DECL_EXPORT
-#  else
-#    define Q_XLSX_EXPORT Q_DECL_IMPORT
-#  endif
-#else
-#  define Q_XLSX_EXPORT
-#endif
-*/
+ 
 #define Q_XLSX_EXPORT // use static lib
 
 #ifdef XLSX_TEST
