1ba38b370cd348d515a2b299214b43add378c60d
Jay Two <j2doll@gmail.com>
2019-11-01 19:44:58 +0900

    set check point of EG_Anchor and sub-objects

--- QXlsx/source/xlsxdrawing.cpp
+++ QXlsx/source/xlsxdrawing.cpp
@@ -62,19 +62,38 @@ void Drawing::saveToXmlFile(QIODevice *device) const
     writer.writeEndDocument();
 }
 
+// check point
 bool Drawing::loadFromXmlFile(QIODevice *device)
 {
+    /*
+    <xsd:group name="EG_Anchor">
+        <xsd:choice>
+            <xsd:element name="twoCellAnchor" type="CT_TwoCellAnchor"/>
+            <xsd:element name="oneCellAnchor" type="CT_OneCellAnchor"/>
+            <xsd:element name="absoluteAnchor" type="CT_AbsoluteAnchor"/>
+        </xsd:choice>
+    </xsd:group>
+    */
+
     QXmlStreamReader reader(device);
-    while (!reader.atEnd()) {
+
+    while (!reader.atEnd())
+    {
         reader.readNextStartElement();
-        if (reader.tokenType() == QXmlStreamReader::StartElement) {
-            if (reader.name() == QLatin1String("absoluteAnchor")) {
+        if (reader.tokenType() == QXmlStreamReader::StartElement)
+        {
+            if (reader.name() == QLatin1String("absoluteAnchor")) // CT_AbsoluteAnchor
+            {
                 DrawingAbsoluteAnchor * anchor = new DrawingAbsoluteAnchor(this);
                 anchor->loadFromXml(reader);
-            } else if (reader.name() == QLatin1String("oneCellAnchor")) {
+            }
+            else if (reader.name() == QLatin1String("oneCellAnchor")) // CT_OneCellAnchor
+            {
                 DrawingOneCellAnchor * anchor = new DrawingOneCellAnchor(this);
                 anchor->loadFromXml(reader);
-            } else if (reader.name() == QLatin1String("twoCellAnchor")) {
+            }
+            else if (reader.name() == QLatin1String("twoCellAnchor")) // CT_TwoCellAnchor
+            {
                 DrawingTwoCellAnchor * anchor = new DrawingTwoCellAnchor(this);
                 anchor->loadFromXml(reader);
             }
