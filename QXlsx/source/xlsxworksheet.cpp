ff396b80db4c77324cc2e8df0261a4a54f646362
NikkiKurashov <84013639+NikkiKurashov@users.noreply.github.com>
2021-05-15 14:49:28 +0800

    Update xlsxworksheet.cpp

--- QXlsx/source/xlsxworksheet.cpp
+++ QXlsx/source/xlsxworksheet.cpp
@@ -3135,7 +3135,7 @@ void WorksheetPrivate::validateDimension()
  */
 SharedStrings *WorksheetPrivate::sharedStrings() const
 {
-	return workbook->sharedStrings();
+    return workbook->sharedStrings();
 }
 
 QVector<CellLocation> Worksheet::getFullCells(int* maxRow, int* maxCol)
@@ -3205,125 +3205,78 @@ QVector<CellLocation> Worksheet::getFullCells(int* maxRow, int* maxCol)
 }
 
 /*!
- * Returns the index of the paper size used for printing
- *
- * 1 = Letter paper (8.5 in. by 11 in.)
- * 2 = Letter small paper (8.5 in. by 11 in.)
- * 3 = Tabloid paper (11 in. by 17 in.)
- * 4 = Ledger paper (17 in. by 11 in.)
- * 5 = Legal paper (8.5 in. by 14 in.)
- * 6 = Statement paper (5.5 in. by 8.5 in.)
- * 7 = Executive paper (7.25 in. by 10.5 in.)
- * 8 = A3 paper (297 mm by 420 mm)
- * 9 = A4 paper (210 mm by 297 mm)
- * 10 = A4 small paper (210 mm by 297 mm)
- * 11 = A5 paper (148 mm by 210 mm)
- * 12 = B4 paper (250 mm by 353 mm)
- * 13 = B5 paper (176 mm by 250 mm)
- * 14 = Folio paper (8.5 in. by 13 in.)
- * 15 = Quarto paper (215 mm by 275 mm)
- * 16 = Standard paper (10 in. by 14 in.)
- * 17 = Standard paper (11 in. by 17 in.)
- * 18 = Note paper (8.5 in. by 11 in.)
- * 19 = #9 envelope (3.875 in. by 8.875 in.)
- * 20 = #10 envelope (4.125 in. by 9.5 in.)
- * 21 = #11 envelope (4.5 in. by 10.375 in.)
- * 22 = #12 envelope (4.75 in. by 11 in.)
- * 23 = #14 envelope (5 in. by 11.5 in.)
- * 24 = C paper (17 in. by 22 in.)
- * 25 = D paper (22 in. by 34 in.)
- * 26 = E paper (34 in. by 44 in.)
- * 27 = DL envelope (110 mm by 220 mm)
- * 28 = C5 envelope (162 mm by 229 mm)
- * 29 = C3 envelope (324 mm by 458 mm)
- * 30 = C4 envelope (229 mm by 324 mm)
- * 31 = C6 envelope (114 mm by 162 mm)
- * 32 = C65 envelope (114 mm by 229 mm)
- * 33 = B4 envelope (250 mm by 353 mm)
- * 34 = B5 envelope (176 mm by 250 mm)
- * 35 = B6 envelope (176 mm by 125 mm)
- * 36 = Italy envelope (110 mm by 230 mm)
- * 37 = Monarch envelope (3.875 in. by 7.5 in.).
- * 38 = 6 3/4 envelope (3.625 in. by 6.5 in.)
- * 39 = US standard fanfold (14.875 in. by 11 in.)
- * 40 = German standard fanfold (8.5 in. by 12 in.)
- * 41 = German legal fanfold (8.5 in. by 13 in.)
- * 42 = ISO B4 (250 mm by 353 mm)
- * 43 = Japanese double postcard (200 mm by 148 mm)
- * 44 = Standard paper (9 in. by 11 in.)
- * 45 = Standard paper (10 in. by 11 in.)
- * 46 = Standard paper (15 in. by 11 in.)
- * 47 = Invite envelope (220 mm by 220 mm)
- * 50 = Letter extra paper (9.275 in. by 12 in.)
- * 51 = Legal extra paper (9.275 in. by 15 in.)
- * 52 = Tabloid extra paper (11.69 in. by 18 in.)
- * 53 = A4 extra paper (236 mm by 322 mm)
- * 54 = Letter transverse paper (8.275 in. by 11 in.)
- * 55 = A4 transverse paper (210 mm by 297 mm)
- * 56 = Letter extra transverse paper (9.275 in. by 12 in.)
- * 57 = SuperA/SuperA/A4 paper (227 mm by 356 mm)
- * 58 = SuperB/SuperB/A3 paper (305 mm by 487 mm)
- * 59 = Letter plus paper (8.5 in. by 12.69 in.)
- * 60 = A4 plus paper (210 mm by 330 mm)
- * 61 = A5 transverse paper (148 mm by 210 mm)
- * 62 = JIS B5 transverse paper (182 mm by 257 mm)
- * 63 = A3 extra paper (322 mm by 445 mm)
- * 64 = A5 extra paper (174 mm by 235 mm)
- * 65 = ISO B5 extra paper (201 mm by 276 mm)
- * 66 = A2 paper (420 mm by 594 mm)
- * 67 = A3 transverse paper (297 mm by 420 mm)
- * 68 = A3 extra transverse paper (322 mm by 445 mm)
+ * Returns whether printed content should be horizontally centered
  */
-quint32 Worksheet::printPaperSize() const
+bool Worksheet::isPrintHorizontalCentered() const
 {
     Q_D(const Worksheet);
-    return d->pageSetup.paperSize;
+    return d->printOptions.horizontalCentered;
 }
 
 /*!
- * Sets the index of the paper size used for printing; see printPaperSize function for known indexes
+ * Sets or unsets horizontal centering of printed content
  */
-void Worksheet::setPrintPaperSize(quint32 paperSizeIdx)
+void Worksheet::setPrintHorizontalCentered(bool centered)
 {
     Q_D(Worksheet);
-    if (paperSizeIdx) d->pageSetup.paperSize = paperSizeIdx;
+    d->printOptions.horizontalCentered = centered;
 }
 
-
 /*!
- * Returns the orientation of printed pages
+ * Returns whether printed content should be vertically centered
  */
-Worksheet::PrintOrientation Worksheet::printOrientation() const
+bool Worksheet::isPrintVerticalCentered() const
 {
     Q_D(const Worksheet);
-    return d->pageSetup.orientation;
+    return d->printOptions.verticalCentered;
 }
 
 /*!
- * Sets the orientation of printed pages
+ * Sets or unsets vertical centering of printed content
  */
-void Worksheet::setPrintOrientation(PrintOrientation orientation)
+void Worksheet::setPrintVerticalCentered(bool centered)
 {
     Q_D(Worksheet);
-    d->pageSetup.orientation = orientation;
+    d->printOptions.verticalCentered = centered;
 }
 
 /*!
- * Returns number of horizontal pages to fit on
+ * Returns whether headings should be printed
  */
-quint32 Worksheet::printFitToWidth() const
+bool Worksheet::arePrintHeadingsVisible() const
 {
     Q_D(const Worksheet);
-    return d->pageSetup.fitToWidth;
+    return d->printOptions.headings;
 }
 
 /*!
- * Sets number of horizontal pages to fit on
+ * Sets or unsets printing of headings
  */
-void Worksheet::setPrintFitToWidth(quint32 fitToWidth)
+void Worksheet::setPrintHeadingsVisible(bool visible)
 {
     Q_D(Worksheet);
-    d->pageSetup.fitToWidth = fitToWidth;
+    d->printOptions.headings = visible;
+}
+
+/*!
+ * Returns whether gridlines should be printed
+ */
+bool Worksheet::arePrintGridLinesVisible() const
+{
+    Q_D(const Worksheet);
+    return d->printOptions.gridLines && d->printOptions.gridLinesSet;
+}
+
+/*!
+ * Sets or unsets printing of gridlines
+ */
+void Worksheet::setPrintGridLinesVisible(bool visible)
+{
+    Q_D(Worksheet);
+    // both options must be set for grid lines to be printed
+    // if any of these options is unset grid lines won't be printed
+    d->printOptions.gridLines = visible;
+    if (visible) d->printOptions.gridLinesSet = visible;
 }
 
 /*!
@@ -3433,6 +3386,344 @@ void Worksheet::setPrintFooterMargin(double margin)
     Q_D(Worksheet);
     d->pageMargins.footer = margin;
 }
+
+/*!
+ * Returns the index of the paper size used for printing
+ *
+ * 1 = Letter paper (8.5 in. by 11 in.)
+ * 2 = Letter small paper (8.5 in. by 11 in.)
+ * 3 = Tabloid paper (11 in. by 17 in.)
+ * 4 = Ledger paper (17 in. by 11 in.)
+ * 5 = Legal paper (8.5 in. by 14 in.)
+ * 6 = Statement paper (5.5 in. by 8.5 in.)
+ * 7 = Executive paper (7.25 in. by 10.5 in.)
+ * 8 = A3 paper (297 mm by 420 mm)
+ * 9 = A4 paper (210 mm by 297 mm)
+ * 10 = A4 small paper (210 mm by 297 mm)
+ * 11 = A5 paper (148 mm by 210 mm)
+ * 12 = B4 paper (250 mm by 353 mm)
+ * 13 = B5 paper (176 mm by 250 mm)
+ * 14 = Folio paper (8.5 in. by 13 in.)
+ * 15 = Quarto paper (215 mm by 275 mm)
+ * 16 = Standard paper (10 in. by 14 in.)
+ * 17 = Standard paper (11 in. by 17 in.)
+ * 18 = Note paper (8.5 in. by 11 in.)
+ * 19 = #9 envelope (3.875 in. by 8.875 in.)
+ * 20 = #10 envelope (4.125 in. by 9.5 in.)
+ * 21 = #11 envelope (4.5 in. by 10.375 in.)
+ * 22 = #12 envelope (4.75 in. by 11 in.)
+ * 23 = #14 envelope (5 in. by 11.5 in.)
+ * 24 = C paper (17 in. by 22 in.)
+ * 25 = D paper (22 in. by 34 in.)
+ * 26 = E paper (34 in. by 44 in.)
+ * 27 = DL envelope (110 mm by 220 mm)
+ * 28 = C5 envelope (162 mm by 229 mm)
+ * 29 = C3 envelope (324 mm by 458 mm)
+ * 30 = C4 envelope (229 mm by 324 mm)
+ * 31 = C6 envelope (114 mm by 162 mm)
+ * 32 = C65 envelope (114 mm by 229 mm)
+ * 33 = B4 envelope (250 mm by 353 mm)
+ * 34 = B5 envelope (176 mm by 250 mm)
+ * 35 = B6 envelope (176 mm by 125 mm)
+ * 36 = Italy envelope (110 mm by 230 mm)
+ * 37 = Monarch envelope (3.875 in. by 7.5 in.).
+ * 38 = 6 3/4 envelope (3.625 in. by 6.5 in.)
+ * 39 = US standard fanfold (14.875 in. by 11 in.)
+ * 40 = German standard fanfold (8.5 in. by 12 in.)
+ * 41 = German legal fanfold (8.5 in. by 13 in.)
+ * 42 = ISO B4 (250 mm by 353 mm)
+ * 43 = Japanese double postcard (200 mm by 148 mm)
+ * 44 = Standard paper (9 in. by 11 in.)
+ * 45 = Standard paper (10 in. by 11 in.)
+ * 46 = Standard paper (15 in. by 11 in.)
+ * 47 = Invite envelope (220 mm by 220 mm)
+ * 50 = Letter extra paper (9.275 in. by 12 in.)
+ * 51 = Legal extra paper (9.275 in. by 15 in.)
+ * 52 = Tabloid extra paper (11.69 in. by 18 in.)
+ * 53 = A4 extra paper (236 mm by 322 mm)
+ * 54 = Letter transverse paper (8.275 in. by 11 in.)
+ * 55 = A4 transverse paper (210 mm by 297 mm)
+ * 56 = Letter extra transverse paper (9.275 in. by 12 in.)
+ * 57 = SuperA/SuperA/A4 paper (227 mm by 356 mm)
+ * 58 = SuperB/SuperB/A3 paper (305 mm by 487 mm)
+ * 59 = Letter plus paper (8.5 in. by 12.69 in.)
+ * 60 = A4 plus paper (210 mm by 330 mm)
+ * 61 = A5 transverse paper (148 mm by 210 mm)
+ * 62 = JIS B5 transverse paper (182 mm by 257 mm)
+ * 63 = A3 extra paper (322 mm by 445 mm)
+ * 64 = A5 extra paper (174 mm by 235 mm)
+ * 65 = ISO B5 extra paper (201 mm by 276 mm)
+ * 66 = A2 paper (420 mm by 594 mm)
+ * 67 = A3 transverse paper (297 mm by 420 mm)
+ * 68 = A3 extra transverse paper (322 mm by 445 mm)
+ */
+quint32 Worksheet::printPaperSize() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.paperSize;
+}
+
+/*!
+ * Sets the index of the paper size used for printing; see printPaperSize function for known indexes
+ */
+void Worksheet::setPrintPaperSize(quint32 paperSizeIdx)
+{
+    Q_D(Worksheet);
+    if (paperSizeIdx) d->pageSetup.paperSize = paperSizeIdx;
+}
+
+/*!
+ * Returns printing scale
+ */
+quint32 Worksheet::printScale() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.scale;
+}
+
+/*!
+ * Sets printing scale the valid values are in the range [ 10 ... 4000 ]
+ */
+void Worksheet::setPrintScale(quint32 scale)
+{
+    Q_D(Worksheet);
+    if (scale >= 10 && scale <= 400) d->pageSetup.scale = scale;
+}
+
+/*!
+ * Returns first page number used for printing
+ */
+quint32 Worksheet::printFirstPageNumber() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.firstPageNumber;
+}
+
+/*!
+ * Sets first page number used for printing
+ */
+void Worksheet::setPrintFirstPageNumber(quint32 firstPage)
+{
+    Q_D(Worksheet);
+    d->pageSetup.firstPageNumber = firstPage;
+}
+
+/*!
+ * Returns number of horizontal pages to fit on
+ */
+quint32 Worksheet::printFitToWidth() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.fitToWidth;
+}
+
+/*!
+ * Sets number of horizontal pages to fit on
+ */
+void Worksheet::setPrintFitToWidth(quint32 fitToWidth)
+{
+    Q_D(Worksheet);
+    d->pageSetup.fitToWidth = fitToWidth;
+}
+
+/*!
+ * Returns number of vertical pages to fit on
+ */
+quint32 Worksheet::printFitToHeight() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.fitToHeight;
+}
+
+/*!
+ * Sets number of vertical pages to fit on
+ */
+void Worksheet::setPrintFitToHeight(quint32 fitToHeight)
+{
+    Q_D(Worksheet);
+    d->pageSetup.fitToHeight = fitToHeight;
+}
+
+/*!
+ * Returns the order of printed pages
+ */
+Worksheet::PrintPageOrder Worksheet::printPageOrder() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.pageOrder;
+}
+
+/*!
+ * Sets the order of printed pages
+ */
+void Worksheet::setPrintPageOrder(PrintPageOrder pageOrder)
+{
+    Q_D(Worksheet);
+    d->pageSetup.pageOrder = pageOrder;
+}
+
+/*!
+ * Returns the orientation of printed pages
+ */
+Worksheet::PrintOrientation Worksheet::printOrientation() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.orientation;
+}
+
+/*!
+ * Sets the orientation of printed pages
+ */
+void Worksheet::setPrintOrientation(PrintOrientation orientation)
+{
+    Q_D(Worksheet);
+    d->pageSetup.orientation = orientation;
+}
+
+/*!
+ * Returns wheather the printing will be black and white
+ */
+bool Worksheet::isPrintBlackAndWhite() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.blackAndWhite;
+}
+
+/*!
+ * Sets wheather the printing will be black and white
+ */
+void Worksheet::setPrintBlackAndWhite(bool blackAndWhite)
+{
+    Q_D(Worksheet);
+    d->pageSetup.blackAndWhite = blackAndWhite;
+}
+
+/*!
+ * Returns wheather the printing will be done without graphics (is draft)
+ */
+bool Worksheet::isPrintDraft() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.draft;
+}
+
+/*!
+ * Sets wheather the printing will be done without graphics (is draft)
+ */
+void Worksheet::setPrintDraft(bool isDraft)
+{
+    Q_D(Worksheet);
+    d->pageSetup.draft = isDraft;
+}
+
+/*!
+ * Returns how cell comments will be printed
+ */
+Worksheet::PrintCellComments Worksheet::printCellComments() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.cellComments;
+}
+
+/*!
+ * Sets how cell comments will be printed
+ */
+void Worksheet::setPrintCellComments(PrintCellComments cellComments)
+{
+    Q_D(Worksheet);
+    d->pageSetup.cellComments = cellComments;
+}
+
+/*!
+ * Returns wheather the first page number will be used
+ */
+bool Worksheet::isPrintUseFirstPageNumber() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.useFirstPageNumber;
+}
+
+/*!
+ * Sets wheather the first page number will be used
+ */
+void Worksheet::setPrintUseFirstPageNumber(bool useFirstPage)
+{
+    Q_D(Worksheet);
+    d->pageSetup.useFirstPageNumber = useFirstPage;
+}
+
+/*!
+ * Returns how errors in cells will be printed
+ */
+Worksheet::PrintErrors Worksheet::printErrors() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.errors;
+}
+
+/*!
+ * Sets how errors in cells will be printed
+ */
+void Worksheet::setPrintErrors(PrintErrors errors)
+{
+    Q_D(Worksheet);
+    d->pageSetup.errors = errors;
+}
+
+/*!
+ * Returns horizontal print resolution
+ */
+quint32 Worksheet::printHorizontalDpi() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.horizontalDpi;
+}
+
+/*!
+ * Sets horizontal print resolution
+ */
+void Worksheet::setPrintHorizontalDpi(quint32 dpi)
+{
+    Q_D(Worksheet);
+    d->pageSetup.horizontalDpi = dpi;
+}
+
+/*!
+ * Returns vertical print resolution
+ */
+quint32 Worksheet::printVerticalDpi() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.verticalDpi;
+}
+
+/*!
+ * Sets vertical print resolution
+ */
+void Worksheet::setprintVerticalDpi(quint32 dpi)
+{
+    Q_D(Worksheet);
+    d->pageSetup.verticalDpi = dpi;
+}
+
+/*!
+ * Returns how many copies will be printed
+ */
+quint32 Worksheet::printCopies() const
+{
+    Q_D(const Worksheet);
+    return d->pageSetup.copies;
+}
+
+/*!
+ * Sets how many copies will be printed
+ */
+void Worksheet::setPrintCopies(quint32 copies)
+{
+    Q_D(Worksheet);
+    d->pageSetup.copies = copies;
+}
+
 
 
 QT_END_NAMESPACE_XLSX
